c1659dab314416fd65ba0c52db34cb7710d7335c
Roberto Vezzoni <roberto.lvezzoni@stone.com.br>
2025-04-11 09:54:40 -0300

    basic numeric types

---
+++ variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/vezzoni/learning_go/variables/chars"
+	"github.com/vezzoni/learning_go/variables/numbers"
+)
+
+func main() {
+	numbers.Example1()
+	numbers.Example2()
+	chars.Example3()
+}
